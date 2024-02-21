06b4b24b94ad7e5d3a17c792ba645b6205a7498f
sherrytang-1138794 <145236375+sherrytang-1138794@users.noreply.github.com>
2024-02-21 00:53:06 -0600

    Add files via upload

---
+++ django_projects/locallibrary/catalog/urls.py
@@ -0,0 +1,58 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
+]
+
+# borrow book
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed')
+]
+
+# renew book
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian')
+]
+
+# edit author
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
+
+# edit book
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
+]
+
+# edit genre
+urlpatterns += [
+    path('genres/', views.GenreListView.as_view(), name='genres'),
+    path('genre/<int:pk>', views.GenreDetailView.as_view(), name='genre-detail'),
+    path('genre/create/', views.GenreCreate.as_view(), name='genre-create'),
+    path('genre/<int:pk>/update/', views.GenreUpdate.as_view(), name='genre-update'),
+    path('genre/<int:pk>/delete/', views.GenreDelete.as_view(), name='genre-delete'),
+]
+
+#edit bookinstance
+urlpatterns += [
+    path('bookinstances/', views.BookInstanceListView.as_view(), name='bookinstances'),
+    path('bookinstance/<uuid:pk>', views.BookInstanceDetailView.as_view(),
+         name='bookinstance-detail'),
+    path('bookinstance/create/', views.BookInstanceCreate.as_view(),
+         name='bookinstance-create'),
+    path('bookinstance/<uuid:pk>/update/',
+         views.BookInstanceUpdate.as_view(), name='bookinstance-update'),
+    path('bookinstance/<uuid:pk>/delete/',
+         views.BookInstanceDelete.as_view(), name='bookinstance-delete'),
+]
