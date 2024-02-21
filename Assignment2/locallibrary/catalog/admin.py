35d2d56724f16a6635b996cd45a0f5b3f8aa4ba3
sherrytang-1138794 <145236375+sherrytang-1138794@users.noreply.github.com>
2024-02-21 12:12:53 -0600

    Add files via upload

---
+++ Assignment2/locallibrary/catalog/admin.py
@@ -0,0 +1,56 @@
+from django.contrib import admin
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+# Define the admin class
+class BooksInline(admin.TabularInline):
+    model = Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name',
+                    'first_name',
+                    'date_of_birth',
+                    'date_of_death')
+    # fields that are displayed
+    fields = ['first_name',
+              'last_name',
+              ('date_of_birth',
+               'date_of_death')]  # displayed horizontally with tuple
+    inlines = [BooksInline]
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book and BookInstance using the decorator
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title',
+                    'author',
+                    'display_genre')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back', 'borrower', 'due_back', 'id')
+    list_display = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
